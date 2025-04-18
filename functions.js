5384c1c7f831958acf5b4aec9592b65c41c5d85f
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-04-17 18:37:51 -0700

    Update functions.js
    logic

--- functions.js
+++ functions.js
@@ -1,10 +1,14 @@
+//ctors
+const checkedLabel = document.querySelector('.switch-toggle input:checked + label');
+bool checkedLabel = true;
+
 //captcha
 function onSubmit(token) {
 document.getElementById("demo-form").submit();
 }
 
-function getElementsByClassName('clicked') {
-  if (clicked != true) {
+function getElementsByClassName('checkedLabel') {
+  if (checkedLabel != true) {
     opacity: 0;
 }
   
